9ccefc7f8231c548f06b199c814559b1a320ba9d
Katharina Knaus <KKnaus@it-economics.de>
2016-09-05 09:09:29 +0200

    Create testcase class GameCharacterizationTest

---
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
@@ -0,0 +1,9 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class GameCharacterizationTest {
+
+}
