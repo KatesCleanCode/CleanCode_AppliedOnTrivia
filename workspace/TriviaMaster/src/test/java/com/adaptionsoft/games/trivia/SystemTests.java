e38395de7c27d287ef928bc422f12df92b1d52ca
Katharina Knaus <KKnaus@it-economics.de>
2016-09-05 12:06:27 +0200

    Refactoring create Named Test Suite SystemTests

---
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/SystemTests.java
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.trivia;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.runner.SelectClasses;
+import org.junit.runner.RunWith;
+
+import com.adaptionsoft.games.trivia.runner.GoldenMasterTest;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses(GoldenMasterTest.class)
+public class SystemTests {
+// executes all system tests
+}
