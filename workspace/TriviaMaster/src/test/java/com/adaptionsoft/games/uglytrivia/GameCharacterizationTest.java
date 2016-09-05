03029eb56ba2dfe99c1587ddc671ba60e4687d09
Katharina Knaus <KKnaus@it-economics.de>
2016-09-05 09:10:38 +0200

    Add failing test: when one player is added

--- workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/uglytrivia/GameCharacterizationTest.java
@@ -1,9 +1,25 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
 public class GameCharacterizationTest {
 
+ @Test
+ void whenOnePlayerIsAddedThenSomethingHappens() {
+  ByteArrayOutputStream output = new ByteArrayOutputStream();
+  System.setOut(new PrintStream(output));
+  Game game = new Game();
+  game.add("John");
+
+  Assert.assertThat(output.toString(),
+   CoreMatchers.equalTo("something happened"));
+ }
 }
