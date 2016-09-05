c7660ef71b3b08e54bf81696b7cae28c4937b072
Katharina Knaus <KKnaus@it-economics.de>
2016-09-05 09:45:18 +0200

    Refactoring create NamedTestSuite for Test AutomPyramid-IntegrationTests

---
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/IntegrationTests.java
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.trivia;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.runner.SelectClasses;
+import org.junit.runner.RunWith;
+
+import com.adaptionsoft.games.uglytrivia.GameCharacterizationTest;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses(GameCharacterizationTest.class)
+public class IntegrationTests {
+// executes all integration tests
+}
