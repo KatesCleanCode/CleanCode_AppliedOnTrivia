b20b4f03440ef54efb22754e13eea25ae0adaa91
Katharina Knaus <KKnaus@it-economics.de>
2016-09-05 12:15:07 +0200

    Create Test Helper for Test Utility Methods - GoldenMasterSupplier

---
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/utils/goldenMaster/GoldenMasterSupplier.java
@@ -0,0 +1,8 @@
+package com.adaptionsoft.games.utils.goldenMaster;
+
+public final class GoldenMasterSupplier {
+
+ private GoldenMasterSupplier() {
+// Utility class must not be instantiated
+ }
+}
