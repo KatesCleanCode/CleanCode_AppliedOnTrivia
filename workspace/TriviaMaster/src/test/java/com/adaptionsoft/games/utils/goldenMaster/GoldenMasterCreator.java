f4b318365bb1cef97a77172882bbeb1cdd84cb82
Katharina Knaus <KKnaus@it-economics.de>
2016-09-05 11:30:43 +0200

    Create GoldenMasterCreator and Golden Master files

---
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/utils/goldenMaster/GoldenMasterCreator.java
@@ -0,0 +1,38 @@
+package com.adaptionsoft.games.utils.goldenMaster;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+
+@RunWith(JUnitPlatform.class)
+public class GoldenMasterCreator {
+
+ @Test
+ public void createGoldenMaster() throws IOException {
+  Path path = Paths.get("src/test/resources/goldenmasterData");
+  if (Files.notExists(path)) {
+   Files.createDirectories(path);
+  }
+  for (int seed = 0; seed < 10_000; seed++) {
+   ByteArrayOutputStream output = new ByteArrayOutputStream();
+   System.setOut(new PrintStream(output));
+   GameRunner.playGame(new Random(seed));
+
+   String gameOutput = output.toString();
+   Path pathToGoldenMaster =
+    Paths.get("src/test/resources/goldenmasterData", seed + ".txt");
+   Path newFile = Files.createFile(pathToGoldenMaster);
+   Files.write(newFile, gameOutput.getBytes());
+  }
+ }
+}
