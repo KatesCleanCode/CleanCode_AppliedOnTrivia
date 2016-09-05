5ab6593d698ff984553dd348aaccb347fec015e4
Katharina Knaus <KKnaus@it-economics.de>
2016-09-05 11:19:39 +0200

    Refactoring Extract Method for Making SUT testable - GameRunner.playGame

--- workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -11,6 +11,10 @@ public class GameRunner {
 
  public static void main(String[] args) {
   Random rand = new Random();
+  playGame(rand);
+ }
+
+ public static void playGame(Random rand) {
   Game aGame = new Game();
   aGame.add("Chet");
   aGame.add("Pat");
