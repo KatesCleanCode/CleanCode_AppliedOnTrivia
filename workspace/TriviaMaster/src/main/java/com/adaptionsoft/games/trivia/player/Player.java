fcf8630402b95405bd4e24ce9284488404cde82e
Katharina Knaus <KKnaus@it-economics.de>
2016-09-06 19:31:26 +0200

    Make last failing test green using Obvious Implementation

--- workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/player/Player.java
+++ workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/player/Player.java
@@ -4,6 +4,7 @@ public class Player {
 
  private String playerName;
  private int location = 0;
+ private int pursus = 0;
 
  public Player(String playerName) {
   this.playerName = playerName;
@@ -15,5 +16,9 @@ public class Player {
 
  public int getLocation() {
   return location;
+ }
+
+ public int getPursus() {
+  return pursus;
  }
 }
