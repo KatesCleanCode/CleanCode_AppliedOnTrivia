66b79ab680515935de3b47f5461077cc6359240b
Katharina Knaus <KKnaus@it-economics.de>
2016-09-06 18:31:31 +0200

    Refactoring create named test suite UnitTests and integrate PlayersTest

---
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/UnitTests.java
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.trivia;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.runner.SelectClasses;
+import org.junit.runner.RunWith;
+
+import com.adaptionsoft.games.trivia.player.PlayersTest;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses(PlayersTest.class)
+public class UnitTests {
+// executes all unit tests
+}
