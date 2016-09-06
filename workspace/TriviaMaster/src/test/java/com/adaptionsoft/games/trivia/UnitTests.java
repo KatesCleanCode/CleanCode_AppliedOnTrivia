091782b0bc9dcfb24d6dd463658111f76d694225
Katharina Knaus <KKnaus@it-economics.de>
2016-09-06 19:28:02 +0200

    Create testcase class PlayerTest and integrate it into UnitTests

--- workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/UnitTests.java
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/UnitTests.java
@@ -4,10 +4,11 @@ import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.runner.SelectClasses;
 import org.junit.runner.RunWith;
 
+import com.adaptionsoft.games.trivia.player.PlayerTest;
 import com.adaptionsoft.games.trivia.player.PlayersTest;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses(PlayersTest.class)
+@SelectClasses({ PlayersTest.class, PlayerTest.class })
 public class UnitTests {
 // executes all unit tests
 }
---
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/player/PlayerTest.java
@@ -0,0 +1,9 @@
+package com.adaptionsoft.games.trivia.player;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class PlayerTest {
+
+}
