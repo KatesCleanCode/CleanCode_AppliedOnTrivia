72121171811f9ede6c7f0cda2921b3cbd5e77d9d
Katharina Knaus <KKnaus@it-economics.de>
2016-09-06 18:58:55 +0200

    Refactoring change all methods to use Players.player - Players

--- workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/player/Players.java
+++ workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/player/Players.java
@@ -9,15 +9,15 @@ public class Players {
  private List<Player> player = new ArrayList<>();
 
  public int getNumberOfPlayers() {
-  return playerNames.size();
+  return player.size();
  }
 
  public void add(String playerName) {
-  playerNames.add(playerName);
+  player.add(new Player());
  }
 
  public Player getCurrentPlayer() {
-  if (playerNames.isEmpty()) {
+  if (player.isEmpty()) {
    return null;
   }
   return new Player();
