4e14d05853066c2370631e85d7a7269b3ff3237a
Katharina Knaus <KKnaus@it-economics.de>
2016-09-06 18:38:52 +0200

    Create failing test when player is initialized programming by intention

--- workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/player/PlayersTest.java
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/player/PlayersTest.java
@@ -1,9 +1,17 @@
 package com.adaptionsoft.games.trivia.player;
 
+import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(JUnitPlatform.class)
 public class PlayersTest {
 
+ @Test
+ void whenPlayersIsInitializedThenNumberOfPlayersIsZero() {
+  Players players = new Players();
+  assertThat(players.getNumberOfPlayers(), equalTo(0));
+ }
 }
