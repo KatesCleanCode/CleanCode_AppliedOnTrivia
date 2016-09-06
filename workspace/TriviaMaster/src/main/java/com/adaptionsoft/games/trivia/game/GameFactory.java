de822b4092a40b2331ed29b6aeba8c8b6ba8d08d
Katharina Knaus <KKnaus@it-economics.de>
2016-09-06 21:55:40 +0200

    Refactoring add API description for Deliberate Comments - GameFactory

--- workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/game/GameFactory.java
+++ workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/game/GameFactory.java
@@ -9,6 +9,13 @@ public final class GameFactory {
 // utility class cannot be instantiated
  }
 
+ /**
+  * Creates an instance of a game.
+  *
+  * @param printer
+  *         {@link Printer} to print {@link News}
+  * @return instance of a {@link Game}
+  */
  public static Game createGame(Printer printer) {
   return new Game(printer);
  }
