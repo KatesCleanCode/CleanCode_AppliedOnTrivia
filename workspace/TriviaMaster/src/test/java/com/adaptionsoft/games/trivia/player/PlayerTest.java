30948f25eef7eb5d89745cbfe5b25883271b965a
Katharina Knaus <KKnaus@it-economics.de>
2016-09-06 19:29:08 +0200

    Create failing when player is created location is zero - PlayerTest

--- workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/player/PlayerTest.java
+++ workspace/TriviaMaster/src/test/java/com/adaptionsoft/games/trivia/player/PlayerTest.java
@@ -1,9 +1,19 @@
 package com.adaptionsoft.games.trivia.player;
 
+import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import static org.junit.Assert.assertThat;
 
 @RunWith(JUnitPlatform.class)
 public class PlayerTest {
 
+ @Test
+ void whenPlayerIsCreatedThenLocationIsZero() {
+  Player player = new Player("Martin");
+  assertThat(player.getLocation(), equalTo(0));
+ }
 }
