e155a2442afa6cb95c59af640b50d175880d1641
Katharina Knaus <KKnaus@it-economics.de>
2016-09-07 11:56:04 +0200

    Refactoring remove unused code for YAGNI - Questions

--- workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/questions/Questions.java
+++ workspace/TriviaMaster/src/main/java/com/adaptionsoft/games/trivia/questions/Questions.java
@@ -1,17 +1,9 @@
 package com.adaptionsoft.games.trivia.questions;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class Questions {
-
- private static final int MAX_NUMBER_OF_QUESTIONS = 50;
-
- private LinkedList<String> popQuestions = new LinkedList<>();
- private LinkedList<String> scienceQuestions = new LinkedList<>();
- private LinkedList<String> sportsQuestions = new LinkedList<>();
- private LinkedList<String> rockQuestions = new LinkedList<>();
 
  private Map<Question, Integer> lastNumbers = new HashMap<>();
 
@@ -19,7 +11,6 @@ public class Questions {
   for (Question question : Question.values()) {
    lastNumbers.put(question, 0);
   }
-  initializeQuestions();
  }
 
  public String askQuestion(int location) {
@@ -28,14 +19,5 @@ public class Questions {
   question.setNumber(questionNumber);
   lastNumbers.put(question, questionNumber + 1);
   return question.getName();
- }
-
- private void initializeQuestions() {
-  for (int i = 0; i < MAX_NUMBER_OF_QUESTIONS; i++) {
-   popQuestions.addLast("Pop Question " + i);
-   scienceQuestions.addLast("Science Question " + i);
-   sportsQuestions.addLast("Sports Question " + i);
-   rockQuestions.addLast("Rock Question " + i);
-  }
  }
 }
